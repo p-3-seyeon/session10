1950e3a72ea47335d739e277c582d06257882f38
p-3-seyeon <syp9908@likelion.org>
2021-07-09 19:44:27 +0900

    수정2

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -130,9 +130,5 @@ MEDIA_URL = '/media/'
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'AKIAX66O7XM3VM3ILQAE'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'parkseyeon'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_REGION_NAME = 'ap-northeast-2'
+
  
